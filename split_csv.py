4decf5efbf9fe7a66be7567748b38d3df99e6a49
ross <820584458@qq.com>
2017-08-02 13:32:58 +0800

    do some remove

--- split_csv.py
+++ split_csv.py
@@ -9,10 +9,9 @@ csv_to_split = pd.read_csv(os.path.join(DATA_PATH, FULL_CSV))
 csv_size = len(csv_to_split)
 grouped = csv_to_split.groupby(level=0)
 grouped_list = [grouped.get_group(x) for x in grouped.groups]
-300
 # 318
 print (csv_size)
-train_list = np.random.choice(csv_size, size=200, replace=False)
+train_list = np.random.choice(csv_size, size=280, replace=False)
 test_list = np.setdiff1d(list(range(csv_size)), train_list)
 print(len(train_list))
 print(len(test_list))
